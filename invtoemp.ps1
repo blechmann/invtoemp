d6237edddc05a6c69393dac324b9f53216305b07
blechmann <35830857+blechmann@users.noreply.github.com>
2019-05-28 12:35:46 +0200

    Update invtoemp.ps1

--- invtoemp.ps1
+++ invtoemp.ps1
@@ -11,7 +11,16 @@ Name Process :  345     28  13000  25880   0,86 11272  1 EmpInventory
 
 #>
 
+#Wir möchten den Pfad haben in dem unser PowerShell .ps1 Skript gerade läuft.
 
+function Get-ScriptDirectory {
+    $Invocation = (Get-Variable MyInvocation -Scope 1).Value
+    Split-Path $Invocation.MyCommand.Path
+}
 
+#aktuelle Pfad steht in $installpath
+$installpath = Get-ScriptDirectory
+$softwarepath = "EmpInventory.exe"
+$aufruf = $installpath + $softwarepath
 
-
+Start-Process -FilePath $aufruf 
