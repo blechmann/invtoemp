00c339012dabe0e6aac80d154e334d748bb91f93
blechmann <35830857+blechmann@users.noreply.github.com>
2019-05-28 11:04:04 +0200

    Create invtoemp.ps1

---
+++ invtoemp.ps1
@@ -0,0 +1,17 @@
+###########################
+#Daten Transfer zu Empirum#
+###########################
+<#
+1. Aufruf EmpInventory.exe 
+2. Ablage in INVSCAN
+3. Export an Empirum Server
+4. Daten ins Backup legen. 
+
+Name Process :  345     28  13000  25880   0,86 11272  1 EmpInventory
+
+#>
+
+
+
+
+
