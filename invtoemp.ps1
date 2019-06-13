6d656288c6bcf682a4bf8eb722d65c7c21750a74
blechmann <35830857+blechmann@users.noreply.github.com>
2019-06-13 12:01:15 +0200

    Update invtoemp.ps1

--- invtoemp.ps1
+++ invtoemp.ps1
@@ -11,6 +11,11 @@ Name Process :  345     28  13000  25880   0,86 11272  1 EmpInventory
 
 #>
 
+#Abfrage was gemacht werden soll
+#1 Datenexport
+#2 
+
+
 #Wir möchten den Pfad haben in dem unser PowerShell .ps1 Skript gerade läuft.
 
 function Get-ScriptDirectory {
@@ -18,9 +23,39 @@ function Get-ScriptDirectory {
     Split-Path $Invocation.MyCommand.Path
 }
 
+#Benutzerabfrage was gemacht werden soll 
+
+$abfrage = Read-Host "Was soll gemacht werden? [1] -> PC Inventory starten [2] -> Daten zum Empirumserver übertragen"
+
+if($abfrage -eq 1 ){
 #aktuelle Pfad steht in $installpath
 $installpath = Get-ScriptDirectory
-$softwarepath = "EmpInventory.exe"
+$softwarepath = "_Full.bat"
 $aufruf = $installpath + $softwarepath
 
-Start-Process -FilePath $aufruf 
+Start-Process -Wait $aufruf #| out-file D:\ScanOrdner\$env:computername.xml#OLD!!
+
+}
+else{
+if($abfrage -eq 2){
+#Daten kopieren zum Empirumserver anschließend Backup
+
+$quelle = "D:\InvScan"
+$dirempirum = "\\ITESX0105\empinv$"
+$dirbackup = "D:\InvScan\_Backup"
+$datum = Get-Date -Format yyyyMMdd 
+
+$dateien = Get-ChildItem -Path D:\InvScan\*.xml
+
+foreach($datei in $dateien){
+    Copy-Item $datei.FullName -Destination $dirempirum
+    $newname = $datei.BaseName+"-"+$datum+".xmlold"
+    Move-Item $datei.FullName -Destination $dirbackup\$newname
+}
+Write-Output "Daten wurden kopiert"
+}
+else{
+  Write-Output "FEHLER falsche Eingabe!"  
+  close 
+}
+}
